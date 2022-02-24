91b31d150579528752f139d86a33fce50ac4c83e
Nazim Nasser <nazimnasser94@gmail.com>
2022-02-24 13:41:28 +0200

    steps 1, 2, 3 and 4

---
+++ basic-1/step-3.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Nazim";
+$email = "nazim@gmail.com";
+echo "My name is $name and my email is $email.\n";
+$height = 200;
+$width = 45;
+$area = $height * $width;
+echo "$area\n";
+?>
---
+++ basic-1/step-4.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Nazim";
+$position = "Studying at Codi";
+$github_url = "https://github.com/";
+echo "Hello, I'm ". $name .", I'm a ". $position ." please check my github link " .$github_url ."\n";
+echo "Hello, I'm $name, I'm a $position please check my github link $github_url\n";
+echo 'Hello, I\'m ' .$name. ', I\'m a ' .$position. ' please check my github link ' .$github_url."\n" ;
+echo "Hello, I'm {$name}, I'm a {$position} please check my github link {$github_url}\n";
+?>
