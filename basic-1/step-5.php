e4616d5b171628f508e7d5e9094b25329e644d68
Nazim Nasser <nazimnasser94@gmail.com>
2022-02-24 14:30:39 +0200

    step-5

---
+++ basic-1/step-5.php
@@ -0,0 +1,11 @@
+<?php
+$course_name = "PHP";
+$enrolled_students = 37;
+$price = 20.5;
+$on_discount = true;
+echo "Course title: $course_name
+Enrolled Students: $enrolled_students
+Course price: $price $ USD
+Course on discount: ";
+echo $on_discount ? 'Yes' : 'No';
+?>
