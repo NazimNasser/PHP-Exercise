e66f0f2cc3f6ea34917e063ed68ce8971742d5a7
Nazim Nasser <nazimnasser94@gmail.com>
2022-02-25 20:33:15 +0200

    step-3

---
+++ basic-2/step-3.php
@@ -0,0 +1,11 @@
+<?php
+$var1 = 32;
+$var2 = 45;
+echo "\nThe number before swapping is:\n";
+echo "Number var1 =".$var1." and var2=".$var2."\n";
+$temp = $var1;
+$var1 = $var2;
+$var2 = $temp;
+echo "\nThe number after swapping is: \n";
+echo "Number var1 =".$var1." and var2=".$var2."\n";
+?>
